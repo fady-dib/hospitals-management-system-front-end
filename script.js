b709c22f51776342a1edf6e0c115b27f9f51f5f6
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-13 02:50:32 +0200

    link login to backend

---
+++ script.js
@@ -0,0 +1,60 @@
+// const hospital_pages = {};
+
+// hospital_pages.base_url = "http://localhost:8080/hospitals-backend/";
+
+// hospital_pages.postAPI = async (api_url, api_data, api_token = null) => {
+//     try {
+//         return await axios.post(
+//             api_url,
+//             api_data,
+//             {
+//                 headers: {
+//                     'Authorization': "token " + api_token
+//                 }
+//             }
+//         );
+//     }
+//     catch(error){
+//         console.log('Error from post api')
+//     }
+// }
+
+  // var atpos = email.indexOf('@');
+    // var dotpos = email.lastIndexOf('.');
+    // if (atpos < 1 || dotpos - atpos < 2){
+    //     alert('Please enter correct E-mail')
+    // }
+    // if(password.length < 8){
+    //     alert('Password should contain 8 characters minimum');
+    // }
+
+
+
+const submit = () => {
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+  
+    console.log('abc')
+
+    let data = new FormData();
+    data.append('email', email);
+    data.append('password', password)
+    axios.post('http://localhost:8080/hospitals-backend/login.php', data).then(function(res){
+        if(res.data.response == 'Loggedin' ){
+            if(res.data.response.usertype == 1){
+                location.replace('')
+            }
+            else if(res.data.response.usertype == 2){
+                location.replace('patient.html')
+            }
+            else{
+                location.replace('admin-panel.html');
+            }
+            window.localStorage.setItem('usertype', res.data.response.usertype_id);
+            window.localStorage.setItem('user_id', res.data.response.user_id)
+        }
+    }).catch(function(error){
+        console.log(error)
+    })
+}
+
