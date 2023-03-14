676328aa070f7a6a6f864319c86c94cb8f7159c8
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-14 22:38:48 +0200

    fix onchange bug

--- javascript/patient.js
+++ javascript/patient.js
@@ -1,7 +1,6 @@
 const department_dropdown = document.getElementById('department');
 const room_dropdown = document.getElementById('room');
-
-
+const bed_dropdown = document.getElementById('bed');
 
 window.onload = function () {
     user_id = localStorage.getItem('user_id');
@@ -14,7 +13,6 @@ window.onload = function () {
             let html ="";
             for (i = 0; i < departments.length; i++) {
                 html += `<option value="${departments[i].department_id}">${departments[i].department_name}</option>`
-                
             }
             department_dropdown.insertAdjacentHTML('beforeend',html);
         })
@@ -25,11 +23,14 @@ const fetchRoom = () => {
     let data = new FormData();
     data.append('department_id', department_dropdown.value);
     axios.post('http://localhost:8080/hospitals-backend/room.php', data).then(function(res){
-        let rooms = res.data.rooms;
-        let html = ""
+        const rooms = res.data.rooms;
+        const beds = res.data.beds;
+        let html = "";
         if(res.data.response == 'No available rooms'){
          alert('No available rooms');
          add_btn.setAttribute('disabled', true);
+            html=`<option value="0">No rooms available</option>`;
+            room_dropdown.innerHTML = html;
 
         }
         else{
@@ -37,6 +38,10 @@ const fetchRoom = () => {
             for (i = 0; i < rooms.length; i++) {
                 html += `<option value="${rooms[i].room_id}">${rooms[i].room_number}</option>`
                 room_dropdown.innerHTML = html;
+            }
+            for (i=0; i<beds.length; i++) {
+                html += `<option value="${beds[i].bed_id}">${beds[i].bed_number}</option>`
+
             }
         }
      }).catch(function(error){
